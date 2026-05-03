593c080bc224feb8288eeaa18d82bfde6891659c
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2026-05-03 16:17:33 -0400

    update env

--- db/init.sql
+++ db/init.sql
@@ -147,3 +147,30 @@ BEGIN
     return v_resid;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE FUNCTION initialize()
+RETURNS VOID
+AS $$
+BEGIN
+    CREATE TABLE transformed AS
+    SELECT
+        ogc_fid AS id,
+        ST_CollectionExtract(ST_MakeValid(ST_GeomFromWKB(wkb_geometry))) AS geo,
+        substring(geoid from 1 for 2)::INT AS state,
+        substring(geoid from 3 for 3)::INT AS county,
+        votes_dem AS dem,
+        votes_rep AS rep,
+        votes_total AS total
+    from jerry;
+
+    INSERT INTO meta (state, type, level)
+        SELECT state, 0, 0
+        FROM transformed
+        GROUP BY state;
+
+    INSERT INTO groups (runid, geo, dem, rep, total)
+        SELECT m.runid, t.geo, COALESCE(t.dem, 0), COALESCE(t.rep, 0), COALESCE(t.total, 0)
+        FROM transformed AS t
+        JOIN meta AS m ON t.state = m.state;
+END;
+$$ LANGUAGE plpgsql
